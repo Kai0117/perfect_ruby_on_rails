3fc4e96515d92ebb0eba16ef8a9e24a2fd903edf
= <dvlpasapro@gmail.com>
2025-07-21 12:01:32 +0900

    feature/practice-of-validation-and-enum

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,8 @@
 class Book < ApplicationRecord
-
     enum :sales_status, {
-        reservation: 0, #予約受付
-        now_on_sale: 1, #発売中
-        end_of_print: 2 #販売終了
+        reservation: 0, # 予約受付
+        now_on_sale: 1, # 発売中
+        end_of_print: 2 # 販売終了
     }
 
     scope :costly, -> { where("price > ?", 3000) }
@@ -15,5 +14,5 @@ class Book < ApplicationRecord
 
     validates :name, presence: true
     validates :name, length: { maximum: 25 }
-    validates :price, numericality: { greater_than_or_equal_to: 0}
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
