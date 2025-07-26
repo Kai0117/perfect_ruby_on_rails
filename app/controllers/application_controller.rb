7fd8b41b552fdcc539fce278288c46a9df083ebd
= <dvlpasapro@gmail.com>
2025-07-26 15:47:12 +0900

    practice-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  rescue_from LoginFailed, with: :login_failed
+  allow_browser versions: :modern
+
+  def login_failed
+    render template: "shared/login_failed", statu: 401
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
